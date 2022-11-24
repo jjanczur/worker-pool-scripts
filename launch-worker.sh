0a60ee15fd8517054917155013eae52e3b7b640c
sandipsable111 <40589999+sandipsable111@users.noreply.github.com>
2022-11-24 12:52:43 +0530

    Updated launch_worker.sh file

--- launch-worker.sh
+++ launch-worker.sh
@@ -1,25 +1,92 @@
 #!/bin/bash
 
+
 # Variables
 WORKER_POOLNAME=public
 DEPOSIT=4000000000
 CHAIN=bellecour
 MINETHEREUM=0.0
 HUBCONTRACT=0x3eca1B216A7DF1C7689aEb259fFB83ADFB894E7f
-WORKER_DOCKER_IMAGE_VERSION=7.0.1
-#IEXEC_CORE_HOST=18.185.47.13 #NFT wallet pool
-IEXEC_CORE_HOST=3.66.230.42 # Non NFT wallet pool
+WORKER_DOCKER_IMAGE_VERSION=7.1.0 # NFT Wallet worker
+#WORKER_DOCKER_IMAGE_VERSION=7.0.1 # Non NFT wallet Worker
+IEXEC_CORE_HOST=workerpool.iexecenterprise.com #18.185.47.13 #NFT wallet pool
+#IEXEC_CORE_HOST=3.66.230.42 # Non NFT wallet pool
 IEXEC_CORE_PORT=7001
 IEXEC_SDK_VERSION=7.2.2
 IEXEC_WORKER_SSL_TRUSTSTORE=/usr/lib/jvm/zulu-11/lib/security/cacerts
-IEXEC_WORKER_SSL_TRUSTSTORE_PASSWORD=YaD&@g4U&!bFfiH3yNtz # This is a variable used localy. It can be public so change it!
-IEXEC_CORE_PROTOCOL=http
+IEXEC_WORKER_SSL_TRUSTSTORE_PASSWORD=YaD&@g4U&!bFfiH3yNtz
+IEXEC_CORE_PROTOCOL=https
 IEXEC_TEE_ENABLED=false
 GAS_PRICE_CAP=0
 GAS_PRICE_MULTIPLIER=0
+TOTAL_AVAILABLE_CPUS=1
+WORKER_DEFAULT_CPU=1
 WORKER_AVAILABLE_CPU=1
 CHAIN_URL=https://bellecour.iex.ec
 RESULTS_DIR=/tmp/results
+KEYSTORE_DIR=
+IMPORT_WALLET=no
+
+# Configuration array declaration
+declare -A config
+
+file="/vagrant/worker_config.properties"
+while IFS='=' read -r key value; do
+CLEANEDVALUE=${value//[$'\t\r\n']}
+   config["$key"]="$CLEANEDVALUE"
+done < "$file"
+
+
+#Config Variables
+CREATE_NEW_WALLET=${config["CREATE_NEW_WALLET"]}
+WALLET_PRIVATE_KEY=${config["WALLET_PRIVATE_KEY"]} 
+WORKERWALLETPASSWORD=${config["WALLET_PASSWORD"]}
+WORKER_NAME=${config["WORKER_NAME"]}
+
+
+echo "________________________________________________________________"
+echo "                                                                "
+echo "Before proceeding further please confirm below details are correct: "
+echo "                                                                "
+
+if [ "$WALLET_PRIVATE_KEY" != "" ]; then
+   echo " You want to import existing wallet for private key = $WALLET_PRIVATE_KEY "
+   CREATE_NEW_WALLET=no
+   IMPORT_WALLET=yes
+elif [ "$CREATE_NEW_WALLET" == "YES" ] || [ "$CREATE_NEW_WALL	ET" == "yes" ]; then	
+   echo " You want to create new wallet = $CREATE_NEW_WALLET"
+   IMPORT_WALLET=no
+fi
+
+
+#echo " You want to create new wallet = $CREATE_NEW_WALLET" 
+#echo " You want to import existing wallet for private key = $WALLET_PRIVATE_KEY "
+echo " Your wallet password is = $WORKERWALLETPASSWORD "
+echo " Your public worker name will be = $WORKER_NAME "
+
+#Set CPUs for worker the number is less than by 1 from available CPUS of the machine.
+TOTAL_AVAILABLE_CPUS=$(nproc --all) 
+WORKER_AVAILABLE_CPU=$(expr  $TOTAL_AVAILABLE_CPUS - 1)
+
+if [ "$WORKER_AVAILABLE_CPU" == 0 ]; then
+	WORKER_AVAILABLE_CPU=$WORKER_DEFAULT_CPU
+	echo " Number of CPUs for your worker = $WORKER_AVAILABLE_CPU out of ($TOTAL_AVAILABLE_CPUS)"
+    echo " ( Stting up default CPUs for worker to $WORKER_DEFAULT_CPU )"    
+fi
+
+echo "                                                                "
+echo "________________________________________________________________"
+echo "                                                                "
+
+while [ "$answercontinue" != "yes" ] && [ "$answercontinue" != "no" ]; do
+	read -p "Do you want continue with the above details? [yes/no] " answercontinue
+done	
+
+if [ "$answercontinue" == "no" ]; then
+    #read -p " Press [Enter] to exit..."
+    exit 1
+fi
+
 
 # Function that prints messages
 function message() {
@@ -124,7 +191,7 @@ else
   if [ "$answerdocker" == "yes" ]; then
     message "INFO" "Changing docker mirror..."
     sudo mkdir -p /etc/docker
-    sudo tee /etc/docker/daemon.json <<-'EOF'
+    sudo tee /etc/docker/daemon.json <<-'EOF'   
 {
  "registry-mirrors": ["https://registry.docker-cn.com"]
 }
@@ -196,9 +263,11 @@ else
     if [ "$DISTRO" == "darwin" ]; then
         files=(/Users/$(whoami)/Library/Ethereum/keystore/*)
         mkdir -p /Users/$(whoami)/Library/Ethereum/keystore/
+	KEYSTORE_DIR=/Users/$(whoami)/Library/Ethereum/keystore/
     else
         files=(/home/$(whoami)/.ethereum/keystore/*)
         mkdir -p /home/$(whoami)/.ethereum/keystore/
+        KEYSTORE_DIR=/home/$(whoami)/.ethereum/keystore/
     fi
 
     for ((i=${#files[@]}-1; i>=0; i--)); do
@@ -222,7 +291,7 @@ else
             if [ "$answerwalletuse" == "yes" ]; then
 
                 # Get wallet password and check it with iExec SDK
-                read -p "Please provide the password of wallet $WALLET_ADDR: " WORKERWALLETPASSWORD
+                #read -p "Please provide the password of wallet $WALLET_ADDR: " WORKERWALLETPASSWORD
                 WALLET_FILE=${files[$i]}
                 WALLET_SELECTED=1
 
@@ -245,24 +314,21 @@ else
     done
 
     # If no wallet was selected
-    if [ "$WALLET_SELECTED" == 0 ]; then
-
-        message "INFO" "No wallet was selected."
-        while [ "$answerwalletcreate" != "yes" ] && [ "$answerwalletcreate" != "no" ]; do
-            read -p "Do you want to create a wallet? [yes/no] " answerwalletcreate
-        done
-
+    if [ "$WALLET_SELECTED" == 0 ]; then       
+	    
+		answerwalletcreate=$CREATE_NEW_WALLET
+		
         # If user accepts to create a wallet
-        if [ "$answerwalletcreate" == "yes" ]; then
-
-            # Get wallet password
-            read -p "Please provide a password to create an encrypted wallet: " WORKERWALLETPASSWORD
+        if [ "$answerwalletcreate" = "yes" ]; then
+            message "INFO" "No wallet was selected."			
+			message "INFO" "Creating new wallet with wallet password: $WORKERWALLETPASSWORD"
+			
             rm -fr /tmp/iexec
             mkdir /tmp/iexec
             cd /tmp/iexec
 
             message "INFO" "Getting created wallet info."
-	   # IEXEC_INIT_RESULT=$(iexec wallet create --keystoredir $PWD  --force --raw --password "$WORKERWALLETPASSWORD")
+	   
 	    IEXEC_INIT_RESULT=$(iexec init --force --raw --password "$WORKERWALLETPASSWORD")
             checkExitStatus $? "Can't create a wallet. Failed init."
 
@@ -282,42 +348,63 @@ else
             message "INFO" "Please fill your wallet with minimum $MINETHEREUM ETH and $DEPOSIT nRLC. Then relaunch the script."
             read -p "Press [Enter] to exit..."
             exit 1
+        
+        elif [ "$IMPORT_WALLET" == "yes" ]; then
+		
+			WORKERWALLETPRIVATEKEY=$WALLET_PRIVATE_KEY
+			rm -fr /tmp/iexec
+			mkdir /tmp/iexec
+			cd /tmp/iexec
+			message "INFO" "Importing wallet for private key : $WORKERWALLETPRIVATEKEY"
+			message "INFO" "Getting wallet info."
+                   # IEXEC_INIT_RESULT=$(iexec wallet create --keystoredir $PWD  --force --raw --password "$WORKERWALLETPASSWORD")
+		   IEXEC_INIT_RESULT=$(iexec wallet import "$WORKERWALLETPRIVATEKEY" --password "$WORKERWALLETPASSWORD" --keystoredir . )  #"$KEYSTORE_DIR")
+		    checkExitStatus $? "Can't import a wallet. Failed init, Please provide a valid private key."
+			
 
+			message "INFO" "Moving wallet to the kestore $KEYSTORE_DIR."
+			 $(mv UTC* $KEYSTORE_DIR )
+			 checkExitStatus $? "Can't move a wallet. Failed movement."
+
+			# Get wallet address and wallet file path
+			WALLET_ADDR=$(echo $IEXEC_INIT_RESULT | sed -E "s/.*\"address\":\"([0-9a-zA-Z]+)\".*/\1/g")
+			WALLET_FILE=$(echo $IEXEC_INIT_RESULT | sed -E "s/.*\"fileName\":\"([0-9a-zA-Z\/.-]+)\".*/\1/g")
+			# Replacing node home with current user home
+			if [ "$DISTRO" == "darwin" ]; then
+				WALLET_FILE=$(echo $WALLET_FILE | sed "s/home\/node\/\.ethereum/Users\/$(whoami)\/Library\/Ethereum/g")
+			else
+				WALLET_FILE=$(echo $WALLET_FILE | sed "s/node/$(whoami)/g")
+			fi
+
+			message "INFO" "A wallet with address $WALLET_ADDR was imported in $WALLET_FILE."
+
+			read -p "Press [Enter] to exit..."
+			exit 1          
         else
-            message "INFO" "You cannot launch a worker without a wallet. Exiting..."
+		    message "INFO" "Required values are not set in worker_config.properties file. Please set them all and relaunch the script."
+            message "ERROR" "You cannot launch a worker without a wallet. Exiting..."			
             read -p "Press [Enter] to exit..."
             exit 1
         fi
-    fi
-
-    echo "WALLET FILE: $WALLET_FILE"
+    fi    
 
     message "INFO" "The wallet $WALLET_ADDR with password $WORKERWALLETPASSWORD and path $WALLET_FILE will be used..."
-
-    message "INFO" "Checking wallet balances."
 
     message "INFO" "Init iExec SDK."
     iexec init --force --skip-wallet
     checkExitStatus $? "Can't init iexec sdk."
 
     message "INFO" "chain.json created in $PWD"
- 
-    sed -i '/default/c\   \"default\": \"bellecour\"' chain.json
-
+    #Set defailt chain to bellecour
+    sed -i '/default/c\   \"default\": \"$CHAIN\"' chain.json
+	checkExitStatus $? "Can't set default chain to $CHAIN ."
+	
+    message "INFO" "Updated default chain to $CHAIN "
     cat chain.json
 
     message "INFO" "Adding Hub Contract address."
     sed -i'.temp' -E "s/(\"id\": \"42\")/\1\,\ \"hub\":\"$HUBCONTRACT\"/g" chain.json
-
     checkExitStatus $? "Can't place hub address."
-
-    message "INFO" "Getting wallet info."
-#    WALLETINFO=$(iexec wallet show --raw --wallet-file $(basename $WALLET_FILE) --password "$WORKERWALLETPASSWORD" --chain $CHAIN)
- #   checkExitStatus $? "Can't get wallet info."
-
-    message "INFO" "Getting account info."
- #   ACCOUNTINFO=$(iexec account show --raw --wallet-file $(basename $WALLET_FILE) --password "$WORKERWALLETPASSWORD" --chain $CHAIN)
-  #  checkExitStatus $? "Can't get account info."
 
     # Getting necessary values
     ETHEREUM=$(echo $WALLETINFO | sed -E "s/.*\"ETH\":\"([0-9.]+)\".*/\1/g")
@@ -361,9 +448,9 @@ else
    # fi
 
     # Get worker name
-    while [[ ! "$WORKER_NAME" =~ ^[-_A-Za-z0-9]+$ ]]; do
-      read -p "Enter worker name [only letters, numbers, - and _ symbols]: " WORKER_NAME
-    done
+    #while [[ ! "$WORKER_NAME" =~ ^[-_A-Za-z0-9]+$ ]]; do
+      #read -p "Enter worker name [only letters, numbers, - and _ symbols]: " WORKER_NAME
+    #done
 
     # Get last version and run worker
     message "INFO" "Creating iExec $WORKER_POOLNAME worker..."
@@ -390,6 +477,7 @@ else
 	     --env " IEXEC_WORKER_DOCKER_REGISTRY_ADDRESS_1=${REGISTRY_ADDRESS:-}" \
 	     --env " IEXEC_WORKER_DOCKER_REGISTRY_USERNAME_1=${REGISTRY_USERNAME:-}" \
 	     --env " IEXEC_WORKER_DOCKER_REGISTRY_PASSWORD_1=${REGISTRY_PASSWORD:-}" \
+	     --env " IEXEC_DEVELOPER_LOGGER_ENABLED=False" \
              -v $WALLET_FILE:/iexec-wallet/encrypted-wallet.json \
              -v /tmp/iexec-worker/${WORKER_NAME}:/tmp/iexec-worker/${WORKER_NAME} \
              -v /var/run/docker.sock:/var/run/docker.sock \
@@ -407,6 +495,7 @@ else
       message "INFO" "Starting worker."
       docker start $WORKER_POOLNAME-worker
       message "INFO" "Worker was successfully started."
+	  message "INFO" "If you want to see logs of your worker please relaunch the worker."
     else
       message "INFO" "You can start the worker later with \"docker start $WORKER_POOLNAME-worker\"."
     fi
