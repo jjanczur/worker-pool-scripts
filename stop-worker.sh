e60e56923639dd54188341feb7172b3e11c3098b
Sandip Sabale <sandi.sabale@jadenx.com>
2022-11-25 07:09:41 +0000

    Stop script updated

--- stop-worker.sh
+++ stop-worker.sh
@@ -2,4 +2,4 @@
 echo "------------------------------------------------------------------------"
 echo "[WARNING] Don't forget to store your wallet from the keystore directory!"
 echo "------------------------------------------------------------------------"
-/bin/bash /home/vagrant/workerdrop-scripts-public/launch-worker.sh --remove
+/bin/bash /home/vagrant/worker-pool-scripts/launch-worker.sh --remove
